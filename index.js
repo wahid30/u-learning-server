712873ee0263a034cb677dac6e1e96ae9db98ec7
wahid30 <md.wahidulalamjp2@gmail.com>
2022-10-25 11:29:01 +0600

    data loaded

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require("./data/categories.json");
+const course = require("./data/course.json");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+app.get("/course", (req, res) => {
+  res.send(course);
+});
+
+app.get("/course-category", (req, res) => {
+  res.send(categories);
 });
 
 app.listen(port, () => {
