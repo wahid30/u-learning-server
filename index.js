38d6ef56e3d31e966542f11fd863c7845dd129ea
wahid30 <md.wahidulalamjp2@gmail.com>
2022-10-25 11:31:17 +0600

    filter category data

--- index.js
+++ index.js
@@ -18,11 +18,20 @@ app.get("/course", (req, res) => {
 app.get("/course-category", (req, res) => {
   res.send(categories);
 });
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id == "08") {
+    res.send(course);
+  } else {
+    const selectedCourse = course.filter((n) => n.category_id === id);
+    res.send(selectedCourse);
+  }
+});
 
 app.get("/course/:id", (req, res) => {
   const id = req.params.id;
-  const selectedcourse = course.find((n) => n._id === id);
-  res.send(selectedcourse);
+  const selectedCourse = course.find((n) => n._id === id);
+  res.send(selectedCourse);
 });
 
 app.listen(port, () => {
